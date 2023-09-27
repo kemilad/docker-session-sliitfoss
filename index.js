5a02eb984329e0ff615a84464755f4ab2bf341cc
Kemila De Silva <kemiladesilva@gmail.com>
2023-09-27 18:00:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(3001, function () {
-  console.log("listening on 3001");
+app.listen(3000, function () {
+  console.log("listening on 3000");
 });
 
 app.get("/", (req, res) => {
